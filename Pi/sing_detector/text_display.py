d9e745b4007bcceab293f950b242d453da1bb080
SeppoLipponen <141328566+SeppoLipponen@users.noreply.github.com>
2024-03-14 12:53:44 +0200

    Update text_display.py

--- Pi/sing_detector/text_display.py
+++ Pi/sing_detector/text_display.py
@@ -5,6 +5,6 @@ lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23])
 
 def send(text):
     lcd.clear()
-    current_pos = (0, 0)
+    lcd.cursor_pos = (0, 0)
     lcd.write_string(text)
     
